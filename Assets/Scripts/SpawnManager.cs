328861a865a9e208917f3ba762822d11cf3c8ea3
Safa Andac <8544658+SanD94@users.noreply.github.com>
2021-01-10 10:59:03 +0300

    adds spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject [] objectPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+       InvokeRepeating("GenerateObjects", 1.5f, 1.5f); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void GenerateObjects()
+    {
+        int len = objectPrefabs.Length;
+        int index = Random.Range(0, len);
+        float xLoc = Random.Range(-5f, 5f);
+        float zLoc = Random.Range(5f, 15f);
+        Vector3 newPos = new Vector3(xLoc, 0, zLoc);
+        Instantiate(objectPrefabs[index], newPos, objectPrefabs[index].transform.rotation);
     }
 }
